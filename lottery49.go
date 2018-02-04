f58c316ee3032a99a3f9da96e0130e574f6743b2
Kin Lo <kinabcd@gmail.com>
2018-02-04 13:26:51 +0800

    add six ball for lottery49

--- lottery49.go
+++ lottery49.go
@@ -25,14 +25,16 @@ func (l *Lottery49) GetInfo(s *goquery.Selection) LotteryInfo {
 	strBall3 := s.Find("span[id^=\"M649Control_history1_dlQuery_SNo3_\"]").Eq(0).Text()
 	strBall4 := s.Find("span[id^=\"M649Control_history1_dlQuery_SNo4_\"]").Eq(0).Text()
 	strBall5 := s.Find("span[id^=\"M649Control_history1_dlQuery_SNo5_\"]").Eq(0).Text()
+	strBall6 := s.Find("span[id^=\"M649Control_history1_dlQuery_SNo6_\"]").Eq(0).Text()
 	intBall1, _ := strconv.ParseInt(strBall1, 10, 32)
 	intBall2, _ := strconv.ParseInt(strBall2, 10, 32)
 	intBall3, _ := strconv.ParseInt(strBall3, 10, 32)
 	intBall4, _ := strconv.ParseInt(strBall4, 10, 32)
 	intBall5, _ := strconv.ParseInt(strBall5, 10, 32)
+	intBall6, _ := strconv.ParseInt(strBall6, 10, 32)
 	info.Id = strId
 	info.Date = strDate
-	info.Balls = append(info.Balls, intBall1, intBall2, intBall3, intBall4, intBall5)
+	info.Balls = append(info.Balls, intBall1, intBall2, intBall3, intBall4, intBall5, intBall6)
 	return info
 }
 
